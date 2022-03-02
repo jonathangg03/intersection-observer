93a3d87fa7c4851c390560636791305fb5d0d903
Jonathan García <megabyte9703@gmail.com>
2022-03-02 10:49:25 -0600

    Change styles and config for deploy

--- src/components/Character/index.js
+++ src/components/Character/index.js
@@ -3,8 +3,16 @@ import './index.css'
 export default function Character({ character }) {
   return (
     <li key={character.id} className='character'>
-      <h2>{character.name}</h2>
       <img src={character.image} alt={character.name} />
+      <div>
+        <h2>{character.name}</h2>
+        <ul>
+          <li>Gender: {character.gender}</li>
+          <li>Species: {character.species}</li>
+          <li>Origin: {character.origin.name}</li>
+          <li>Location: {character.location.name}</li>
+        </ul>
+      </div>
     </li>
   )
 }
